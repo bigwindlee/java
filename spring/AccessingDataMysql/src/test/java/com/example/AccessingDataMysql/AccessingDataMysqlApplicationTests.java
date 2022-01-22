688cd8796206025607a860b15bd910164669be8c
bigwindlee <bigwindlee@qq.com>
2022-01-22 21:55:41 +0800

    AccessingDataMysql project generated by initializr

---
+++ spring/AccessingDataMysql/src/test/java/com/example/AccessingDataMysql/AccessingDataMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessingDataMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
